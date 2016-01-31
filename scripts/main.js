19cc4a3ac8149339fdcedf6a79c17e09a98ec8fa
Abuzayd <abuzayd@Roshins-MacBook-Pro.local>
2016-01-31 22:16:21 +0400

    First gulp-webapp dist folder

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";var tl=new TimelineMax({repeat:-1}),tl2=new TimelineMax({repeat:0});tl.staggerFromTo(".circle",.5,{scale:.1,opacity:0},{opacity:1,scale:1.2},.3),tl2.fromTo(".activity",1,{top:50,opacity:0},{top:0,opacity:1},1);
